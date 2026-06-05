fb1ae103da322efa5453c63f61ac3377f3c3cbdd
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:29 -0500

    xml commenting

--- src/Soenneker.OpenAq.OpenApiClientUtil/Abstract/IOpenAqOpenApiClientUtil.cs
+++ src/Soenneker.OpenAq.OpenApiClientUtil/Abstract/IOpenAqOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.OpenAq.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IOpenAqOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<OpenAqOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
